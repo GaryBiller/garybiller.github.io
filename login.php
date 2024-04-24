eddf9b58e92be46f65c1dac569c5b484b1e73b91
GaryBiller <165268578+GaryBiller@users.noreply.github.com>
2024-04-24 16:14:19 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,1 @@
-<?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if ($username === 'username' && $password === 'Q8£764NQVS2X})??w#{KkG[I') {
-        header("location: success.html");
-        exit;
-    } else {
-        echo "Invalid username or password";
-    }
-}
-?>
